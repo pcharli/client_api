a4641d87c47f5340dcafd1fbbc6f7e91818c1603
pierre <pcepegra@gmail.com>
2022-07-08 08:29:32 +0200

    update

--- connexion.js
+++ connexion.js
@@ -1,32 +1,35 @@
 const urlApi = 'http://localhost/ingrwf10/api_php/'
-const connectForm = document.querySelector('.connexion')
+        const connectForm = document.querySelector('.connexion')
+        const deconnectLink = document.querySelector('.deconnexion')
 
-connectForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let ident = {
-        login: document.querySelector('.login').value ,
-        password: document.querySelector('.password').value
-    }
-    console.log(ident)
-    fetch(urlApi + 'auth', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        mode: "no-cors",
-        body: JSON.stringify(ident)
-    })
-    .then(response => {
-        response.json()
-    })
-    .then (response => {
-        console.log(response)
-    })
-    .catch(error => console.error(error))
-})
+        deconnectLink.addEventListener('click', (e) => {
+            e.preventDefault()
+            fetch(urlApi + 'deconnect')
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => console.error(error))
+        })
 
-fetch(urlApi+'deconnect')
-.then(response=>response.json())
-.then(response=>{
-    console.log(response)
-})
+
+        connectForm.addEventListener('submit', (e) => {
+            e.preventDefault()
+            let ident = {
+                login: document.querySelector('.login').value ,
+                password: document.querySelector('.password').value
+            }
+
+            fetch(urlApi + 'auth', {
+                headers: {
+                    "content-type": "application/json"
+                },
+                method: 'POST',
+                body: JSON.stringify(
+                    ident
+                )
+            })
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+        })
